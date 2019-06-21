b3ef6815655e593dc2b87006938b629398b6e472
newtypeV2 <gianlibunao@gmail.com>
2019-06-21 09:43:49 -0400

    Updated/improved the matchmaker UI.

--- app/controllers/matchmaker_controller.rb
+++ app/controllers/matchmaker_controller.rb
@@ -2,15 +2,16 @@ class MatchmakerController < ApplicationController
     before_action :authorized
 
     def index
-        if params[:filter]
-            if params[:filter].downcase=="male" || params[:filter].downcase=="female" || params[:filter].downcase=="other"
-            @users = current_user.filter_available_pool_by_gender(params[:filter].downcase)
-            else
-                redirect_to matchmaker_index_path
-            end
-        else
-            @users = current_user.available_pool
-        end
+        # if params[:filter]
+        #     if params[:filter].downcase=="male" || params[:filter].downcase=="female" || params[:filter].downcase=="other"
+        #     @users = current_user.filter_available_pool_by_gender(params[:filter].downcase)
+        #     else
+        #         redirect_to matchmaker_index_path
+        #     end
+        # else
+        #     @users = current_user.available_pool
+        # end
+        @user = @current_user.available_pool.sample
     end
 
     def clicked_yes
